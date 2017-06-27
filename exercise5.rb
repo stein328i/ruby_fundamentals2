7d2e9e63ee1661670bacb605913c30ae5f70d23e
stein328i <stein.328i@gmail.com>
2017-06-27 14:29:09 -0400

    ex5

---
+++ exercise5.rb
@@ -0,0 +1,18 @@
+
+name = 'Amanda'
+
+def greet_backwards(name)
+  name.reverse
+end
+
+p "hello #{greet_backwards(name)}"
+
+
+
+name2 = 'Bob'
+
+def greet_backwards(name2)
+  name2.reverse
+end
+
+p "hello #{greet_backwards(name2)}"
